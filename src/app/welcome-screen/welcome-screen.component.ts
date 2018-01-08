cf361beb47c32045a20692fd3771598e008a436f
Philip Drozd <me@philipdrozd.com>
2018-01-08 11:07:10 +0100

    added welcome screen

---
+++ src/app/welcome-screen/welcome-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-screen',
+  templateUrl: './welcome-screen.component.html',
+  styleUrls: ['./welcome-screen.component.sass']
+})
+export class WelcomeScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
