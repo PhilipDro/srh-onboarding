95d19e31d3712b0fb612f020a077e7a3e785d686
Philip Drozd <me@philipdrozd.com>
2018-01-11 22:25:13 +0100

    remove blur at the end of tour

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,18 +9,28 @@ export class DashboardComponent implements OnInit {
   backdrop = true;
   blurElement = 0;
 
+  // new function reference is created after bind() --> need to save to variable
+  containerListener;
 
   constructor(private elementRef:ElementRef) { }
 
   onWelcomeGo(clicked: boolean) {
-    // add event listener to main.container to go through tutorial
+    // add event listener to main.container to go through tutorial when wecome "Go" is clicked
+    this.containerListener = this.tapAnywhere.bind(this)
     this.elementRef.nativeElement.querySelector('main.container')
-    .addEventListener('click', this.tapAnywhere.bind(this));
+    .addEventListener('click', this.containerListener);
+
   }
 
   // when user taps anywhere after starting tour
   tapAnywhere() {
     this.blurElement ++;
+    if(this.blurElement == 8) {
+      this.backdrop = false;
+      this.blurElement = -1;
+      this.elementRef.nativeElement.querySelector('main.container')
+      .removeEventListener('click', this.containerListener);
+    }
   }
 
   ngOnInit() {
