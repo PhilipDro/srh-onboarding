d5f7285d9822bbaf037de068533caa5d5b101496
nastasios <nastasios@hotmail.de>
2018-01-30 00:17:29 +0100

    UI chachanges

--- src/app/mock-modules.ts
+++ src/app/mock-modules.ts
@@ -1,10 +1,10 @@
 import { Module } from './module';
 
 export const MODULES: Module[] = [
-  { id: 1, name: 'Waldklinikum', thumbnail: '../assets/images/bild1.jpg', description: 'Hier finden Sie alle wichtigen Informationen über das Klinikum, wo sich die Cafeteria befindet, die Geschichte des Klinikums und das SRH Unternehmen'},
-  { id: 2, name: 'Unterhaltung', thumbnail: '../assets/images/mario-luigi-figures-funny-163114.jpeg', description: 'Hier finden Sie eine große Auswahl an Filmen, Dokumentationen, Videos, sowohl auch endlosen Spielspaß'},
-  { id: 3, name: 'Wochenbett', thumbnail: '../assets/images/newborn-baby-feet-basket-161709.jpeg', description: 'Dies ist ein Test Beschreibungstext. Hier kommen verschiedene Informationen zu dem entsprechenden Modul rein.' },
+  { id: 1, name: 'Waldklinikum', thumbnail: '../assets/images/WKG_300_dpi.jpg', description: 'Hier finden Sie alle wichtigen Informationen über das Klinikum, wo sich die Cafeteria befindet, die Geschichte des Klinikums und das SRH Unternehmen'},
+  { id: 2, name: 'Unterhaltung', thumbnail: '../assets/images/pexels-photo-65128.jpeg', description: 'Hier finden Sie eine große Auswahl an Filmen, Dokumentationen, Videos, sowohl auch endlosen Spielspaß'},
+  { id: 3, name: 'Spiele', thumbnail: '../assets/images/pexels-photo-256428.jpeg', description: 'Dies ist ein Test Beschreibungstext. Hier kommen verschiedene Informationen zu dem entsprechenden Modul rein.' },
   { id: 4, name: 'Nachsorge', thumbnail: '../assets/images/pexels-photo-755028.jpeg', description: 'Tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.' },
   { id: 5, name: 'Mutterstation', thumbnail: '../assets/images/nach.jpeg', description: 'Dies ist ein Test Beschreibungstext. Hier kommen verschiedene Informationen zu dem entsprechenden Modul rein.' },
-  { id: 6, name: 'Gelenkstörungen', thumbnail: '../assets/images/bild3.jpg', description: 'Tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.' },
+  { id: 6, name: 'Wochenbett', thumbnail: '../assets/images/newborn-baby-feet-basket-161709.jpeg', description: 'Tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.' },
 ];
