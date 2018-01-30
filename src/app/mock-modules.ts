3cbe89a66308b68dfce563a8eafb6749fb93febb
Philip Drozd <me@philipdrozd.com>
2018-01-30 12:40:23 +0100

    changed klinikum image, made text more visible

--- src/app/mock-modules.ts
+++ src/app/mock-modules.ts
@@ -1,7 +1,7 @@
 import { Module } from './module';
 
 export const MODULES: Module[] = [
-  { id: 1, name: 'Waldklinikum', thumbnail: '../assets/images/WKG_300_dpi.jpg', description: 'Hier finden Sie alle wichtigen Informationen über das Klinikum, wo sich die Cafeteria befindet, die Geschichte des Klinikums und das SRH Unternehmen'},
+  { id: 1, name: 'Waldklinikum', thumbnail: '../assets/images/klinikum.JPG', description: 'Hier finden Sie alle wichtigen Informationen über das Klinikum, wo sich die Cafeteria befindet, die Geschichte des Klinikums und das SRH Unternehmen'},
   { id: 2, name: 'Unterhaltung', thumbnail: '../assets/images/pexels-photo-65128.jpeg', description: 'Hier finden Sie eine große Auswahl an Filmen, Dokumentationen, Videos, sowohl auch endlosen Spielspaß'},
   { id: 3, name: 'Spiele', thumbnail: '../assets/images/pexels-photo-256428.jpeg', description: 'Dies ist ein Test Beschreibungstext. Hier kommen verschiedene Informationen zu dem entsprechenden Modul rein.' },
   { id: 4, name: 'Nachsorge', thumbnail: '../assets/images/pexels-photo-755028.jpeg', description: 'Tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.' },
