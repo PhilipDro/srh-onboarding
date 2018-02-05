3f9d4fbc29e7e5991eaae954c8f56995642ddd5c
Philip Drozd <me@philipdrozd.com>
2018-02-05 11:33:37 +0100

    new srh trip

--- src/app/srh-tour-game/srh-tour-game.component.ts
+++ src/app/srh-tour-game/srh-tour-game.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./srh-tour-game.component.sass']
 })
 export class SrhTourGameComponent implements OnInit {
+  stage = 1;
+  points = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  next(indicator, event: any) {
+    if(indicator == 'right') {
+      event.target.className += "success";
+      this.points ++;
+      setTimeout(()=>this.incrStage(),2000);
+    }
+    else {
+      this.incrStage();
+    }
+  }
+
+  incrStage() {
+    this.stage ++;
   }
 
 }
